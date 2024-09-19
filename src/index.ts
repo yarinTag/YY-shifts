c2ad774c010ac3ef843d18ac65fd8f70cd50c94d
Yariv Menachem <yariv220895@gmail.com>
2024-09-19 18:17:55 +0300

    Only if the database connection is successful will the Express server start.

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,22 @@ dotenv.config();
 
 const app = express();
 
-// Connect to the PostgreSQL database
+// Connect to the PostgreSQL database once the app starts
 client
   .connect()
   .then(() => {
     console.log('Connected to PostgreSQL database');
+
+    // Start the Express server only after the database connection is successful
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    });
   })
   .catch((err) => {
     console.error('Error connecting to PostgreSQL database:', err);
+    process.exit(1); // Exit the process if the database connection fails
   });
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript + Node.js + Express!');
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
