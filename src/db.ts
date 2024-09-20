be22e1fc41449fb78dcd4274dbdea13820e63de9
yarinTag <taguori5@gmail.com>
2024-09-20 15:51:05 +0300

    casting dbport to number

--- src/db.ts
+++ src/db.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const dataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
-  port: 5432,
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
