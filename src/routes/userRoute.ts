7b0a710602b87d36756fe79feda8dbbf9d49781e
Yariv Menachem <yariv220895@gmail.com>
2024-09-25 15:28:54 +0300

    added as middleware validate request

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
 import { createUser, getAllUsers } from '../modules/users/user.controller';
+import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
+import { validationMiddleware } from '../middlewares/validate';
 
 const router = Router();
 
+class UserCreateRequest {
+  @IsString()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsPhoneNumber()
+  phone: string;
+}
+
 router.get('/users', getAllUsers);
-router.post('/users', createUser);
+router.post('/users', validationMiddleware(UserCreateRequest), createUser);
 // router.get('/users/:id');
 
 export default router;
