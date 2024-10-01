95289d1f5d98a4ddb8c723282b54f36bca1562aa
Yariv Menachem <yariv220895@gmail.com>
2024-10-01 18:02:26 +0300

    created dtos

---
+++ src/modules/workCycleConfiguration/dto/UpdateRequest.ts
@@ -0,0 +1,22 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsUUID } from 'class-validator';
+import { WorkDay } from '../../../types/enum/workDay';
+
+export class UpdateRequest {
+  @IsUUID()
+  id: string;
+  @IsOptional()
+  @IsNumber()
+  cycleDays: number;
+  @IsOptional()
+  @IsNotEmpty()
+  startDay: WorkDay;
+  @IsOptional()
+  @IsNumber()
+  legalAmountOfWorkDays: number;
+  @IsOptional()
+  @IsNumber()
+  amountOfDayOff: number;
+  @IsOptional()
+  @IsUUID()
+  departmentId: string;
+}
