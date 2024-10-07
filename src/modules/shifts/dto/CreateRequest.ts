3e1a2ea60e6929f248cb789f7a5b2cab013f3a9b
yarinTag <taguori5@gmail.com>
2024-10-07 11:31:44 +0300

    validation with in not empty object

--- src/modules/shifts/dto/CreateRequest.ts
+++ src/modules/shifts/dto/CreateRequest.ts
@@ -1,12 +1,14 @@
-import { IsBoolean, IsUUID } from 'class-validator';
+import { IsBoolean, IsNotEmptyObject, IsUUID } from 'class-validator';
 import { LocalDateTime } from '@js-joda/core';
 
 export class CreateRequest {
   @IsBoolean()
   active: boolean;
 
+  @IsNotEmptyObject()
   start: LocalDateTime;
 
+  @IsNotEmptyObject()
   end: LocalDateTime;
 
   @IsUUID()
