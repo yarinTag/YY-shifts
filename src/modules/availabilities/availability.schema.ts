a4688649a2dbee400487cd1d12b0e7a1cf640e7c
Yariv Menachem <yariv220895@gmail.com>
2024-10-09 13:27:56 +0300

    updated the id to be multi primary

--- src/modules/availabilities/availability.schema.ts
+++ src/modules/availabilities/availability.schema.ts
@@ -1,24 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from 'typeorm';
+import { Entity, ManyToOne, Column, PrimaryColumn } from 'typeorm';
 import { User } from '../users/user.schema';
 import { Shift } from '../shifts/shift.schema';
 import { BaseEntity } from '../BaseEntity';
 
 @Entity()
 export class Availability extends BaseEntity {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
+  @PrimaryColumn('uuid')
+  userId: string;
+
+  @PrimaryColumn('uuid')
+  shiftId: string;
 
   @Column({ type: 'boolean', default: true })
   active: boolean;
 
   @Column('text')
   memo: string;
-
-  @Column({ name: 'user_id' })
-  userId: string;
-
-  @Column({ name: 'shift_id' })
-  shiftId: string;
 
   @ManyToOne(() => User, (user) => user.availabilities)
   user: User;
