d5bfdc61778f8a4720e6256b57a7063d3fb3ad21
yarinTag <taguori5@gmail.com>
2024-10-21 01:14:37 +0300

    all endpoints

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,36 +1,7 @@
 import swaggerAutogen from 'swagger-autogen';
 
 const outputFile = './swagger-output.json';
-const endpointsFiles = [
-  './src/routes/userRoute.ts',
-  './src/routes/departmentRoute.ts',
-  './src/routes/shiftConfigurationRoute.ts',
-  // './src/routes/workCycleConfigurationRoute.ts',
-  // './src/routes/availabilityRoute.ts',
-  // './src/routes/shiftRoute.ts',
-  // './src/routes/workCycleRoute.ts',
-];
-
-export const extractTags = (filePath: string) => {
-  const baseName = filePath.split('/').pop()?.replace('.ts', '');
-  if (baseName) {
-    return [baseName.charAt(0).toUpperCase() + baseName.slice(1) + 's'];
-  }
-  return [];
-};
-
-// Create a set to store unique tags
-const tagsSet = new Set<string>();
-
-// Loop through endpoint files to populate tags
-endpointsFiles.forEach(filePath => {
-  const tags = extractTags(filePath);
-  tags.forEach(tag => tagsSet.add(tag)); // Add tags to the set
-});
-
-// Convert the set to an array for Swagger documentation
-const tagsArray = Array.from(tagsSet).map(tag => ({ name: tag, description: `${tag} management` }));
-
+const endpointsFiles = ['./src/index.ts'];
 
 const doc = {
   info: {
@@ -43,8 +14,14 @@ const doc = {
       url: 'http://localhost:3000',
     },
   ],
-  tags: tagsArray,
-
+  securityDefinitions: {
+    apiKeyAuth: {
+      type: 'apiKey',
+      in: 'cookie', // can be 'header', 'query' or 'cookie'
+      name: 'token', // name of the header, query parameter or cookie
+      description: 'Set token here...'
+    }
+  },
 };
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
